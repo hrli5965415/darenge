f961be2a879c6a6570af3f958868f71e54704e49
hrli <hrli@mac.local>
2022-09-03 23:44:23 +0800

    fifth commit

--- utils/utility.js
+++ utils/utility.js
@@ -11,7 +11,7 @@ export const convertThemeToObj = (theme) => {
 };
 
 export const nameToUrl = (name) => {
-    return name.toString().toLowerCase().replaceAll(/\s/g, '-')
+    return name.toString().toLowerCase().replace(/\s/g, '-')
 }
 
 export const urlToName = (url) => {
